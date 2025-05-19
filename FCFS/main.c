b93fbba4f439d72f262af548b62d2d7e84a879c4
Masfiq Reza <rezamasfiq@gmail.com>
2025-05-18 18:07:22 -0600

    Add files via upload

---
+++ FCFS/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int numberOfProcesses;
+    printf("Enter how many processes: \n");
+    scanf("%d", &numberOfProcesses);
+    printf("%d",numberOfProcesses);
+
+
+
+    return 0;
+}
